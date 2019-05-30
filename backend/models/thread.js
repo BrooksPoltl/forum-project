eed67cd150a920ca1525fdac5d3ca69d2bd58b3b
BrooksPoltl <brookspoltl@yahoo.com>
2019-05-30 17:41:22 -0500

    created thread type

--- backend/models/thread.js
+++ backend/models/thread.js
@@ -10,3 +10,34 @@
 //   }
 
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId= mongoose.Schema.types.ObjectId
+const threadSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    upvotes: {
+        type: Array,
+        required: true
+    },
+    downvotes: {
+        type: Array,
+        required: true
+    },
+    userId: {
+        type: ObjectId,
+        required: true
+    },
+    topicId: {
+        type: ObjectId,
+        required: true
+    }
+},{
+    timestamps: true
+})
+module.exports = mongoose.model('thread', threadSchema)
