41f5a2c4dd80a0b9740cc8209d7873acbbf4e05e
BrooksPoltl <brookspoltl@yahoo.com>
2019-05-30 17:36:01 -0500

    finished topic model

---
+++ backend/models/thread.js
@@ -0,0 +1,12 @@
+// {
+//     title: string,
+//     description: string,
+//     upvotes:[userIds],
+//     downvotes: [userIds],
+//     comments:[comment]
+//     createdAt: time,
+//     userId: int,
+//     topicId: int
+//   }
+
+const mongoose = require('mongoose');
---
+++ backend/models/topic.js
@@ -0,0 +1,29 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const topicSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    subscribers: {
+        type: Number,
+        required: true
+    },
+    threads: {
+        type: Array,
+        required: true
+    },
+    leader: {
+        type: ObjectId,
+        required: true
+    },
+    moderators: {
+        type: Array,
+        required: true
+    }
+})
+
+module.exports = mongoose.model('topic', topicSchema)
