086653bbc669fd2c54b83f59546fbd7a47722619
BrooksPoltl <brookspoltl@yahoo.com>
2019-05-30 16:42:58 -0500

    setup mongo db

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(cors())
 
 const port = process.env.PORT || 4000;
-app.listen(port, ()=>{
-    console.log(`listening of port ${port}`)
+const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-t6a6m.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`;
+
+mongoose.connect(uri).then(()=>{
+    app.listen(port, ()=>{
+        console.log(`listening of port ${port}`)
+    })
+}).catch(err=>{
+    console.log(err);
 })
