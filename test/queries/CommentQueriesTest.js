005009a143df6f0650364773d13b7c2bea012edb
BrooksPoltl <bpoltl1@gmail.com>
2019-06-09 17:43:49 -0500

    comment test

--- test/queries/CommentQueriesTest.js
+++ test/queries/CommentQueriesTest.js
@@ -116,6 +116,122 @@ describe('Comment Queries', ()=>{
         })
     })
     describe('Comment', ()=> {
-
+        describe('basic comments query', ()=>{
+            it('should be valid query', ()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should be invalid query', ()=>{
+                const query = `
+                    {
+                        comment(id: ""){
+                            _id
+                            das
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
+            it('should take id', ()=>{
+                const query = `
+                    {
+                        comment{
+                            _id
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
+        })
+        describe('access users from upvotes', ()=>{
+            it('should be able to access user data',()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                            upvotes{
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(true,query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                            upvotes{
+                                userNam
+                            }
+                        }
+                    }
+                `
+                tester.test(false,query)
+            })
+        })
+        describe('access users from downvotes', ()=>{
+            it('should be able to access user data',()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                            downvotes{
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(true,query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                            downvotes{
+                                userNam
+                            }
+                        }
+                    }
+                `
+                tester.test(false,query)
+            })
+        })
+        describe('access thread', ()=>{
+            it('should be able to access thread data',()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                            thread{
+                                title
+                            }
+                        }
+                    }
+                `
+                tester.test(true,query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        comment(id:""){
+                            _id
+                            thread{
+                                titl
+                            }
+                        }
+                    }
+                `
+                tester.test(false,query)
+            })
+        })
     })
 })
