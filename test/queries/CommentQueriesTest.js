f9a1f9095e6104950fc85a07c5b9056fbf8bf481
BrooksPoltl <bpoltl1@gmail.com>
2019-06-09 17:33:34 -0500

    added thread test

---
+++ test/queries/CommentQueriesTest.js
@@ -0,0 +1,14 @@
+const schemaCode = require('../../backend/schema/schema')
+const EasyGraphQLTester = require('easygraphql-tester')
+
+describe('Comment Queries', ()=>{
+    describe('Comments', ()=>{
+        describe('basic comments query', ()=>{
+
+        })
+        
+    })
+    describe('Comment', ()=> {
+
+    })
+})
--- test/queries/ThreadQueriesTest.js
+++ test/queries/ThreadQueriesTest.js
@@ -148,19 +148,152 @@ describe('Thread Queries',()=>{
     })
     describe('Thread',()=>{
         describe('basic thread query',()=>{
-
+            it('should be valid query',()=>{
+                const query = `
+                    {
+                        thread(id: ""){
+                            _id
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should be invalid query',()=>{
+                const query = `
+                    {
+                        thread(id:""){
+                            _id
+                            us
+                        }
+                    }
+                `   
+                tester.test(false, query)
+            })
+            it('must have id param', ()=>{
+                const query = `
+                    {
+                        thread{
+                            _id
+                        }
+                    }
+                `   
+                tester.test(false, query)
+            })
         })
         describe('access users from upvotes',()=>{
-            
+            it('should get user data',()=>{
+                const query = `
+                    {
+                        thread(id: ""){
+                            _id
+                            upvotes{
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        threads(id: ""){
+                            _id
+                            upvotes{
+                                userName
+                                title
+                            }
+                        }
+                    }
+                `   
+                tester.test(false, query)
+            })
         })
         describe('access users from downvotes',()=>{
-            
+            it('should get user data',()=>{
+                const query = `
+                    {
+                        thread(id: ""){
+                            _id
+                            downvotes{
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        thread(id: ""){
+                            _id
+                            downvotes{
+                                userName
+                                title
+                            }
+                        }
+                    }
+                `   
+                tester.test(false, query)
+            })
         })
         describe('access comments from thread',()=>{
-            
+            it('should get comment data',()=>{
+                const query = `
+                    {
+                        thread(id:""){
+                            _id
+                            comments{
+                                content
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        thread(id:""){
+                            _id
+                            comments{
+                                content
+                                title
+                            }
+                        }
+                    }
+                `   
+                tester.test(false, query)
+            })
         })
         describe('access topic from thread',()=>{
-            
+            it('should get topic data',()=>{
+                const query = `
+                    {
+                        thread(id:""){
+                            _id
+                            topic{
+                                name
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        thread(id:""){
+                            _id
+                            topic{
+                                userName 
+                            }
+                        }
+                    }
+                `   
+                tester.test(false, query)
+            })
         })
     })
     
