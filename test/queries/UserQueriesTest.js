422fbcb107a03f005b24446b732e8eac0a2bee41
BrooksPoltl <bpoltl1@gmail.com>
2019-06-09 16:14:53 -0500

    finished user queries test

--- test/queries/UserQueriesTest.js
+++ test/queries/UserQueriesTest.js
@@ -10,16 +10,254 @@ describe('User Queries', ()=>{
         tester = new EasyGraphQLTester(schemaCode)
     })
     describe('users', ()=>{
-        it('should return user',()=>{
-            const query = `
-                {
-                    users{
-                        _id
-                        userName
+        describe('making basic query', ()=>{
+            it('should be valid query',()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                        }
                     }
-                }
-            `
-            tester.test(true, query)
+                `
+                tester.test(true, query)
+            })
+            it('should be invalid query',()=>{
+                const query = `
+                    {
+                        getusers{
+                            _id
+                            userName
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
+        })
+        describe('accessing comments from users',()=>{
+            it('Should allow you to access comments', ()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                            comments{
+                                _id
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('Should not return userName on comments', ()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                            comments{
+                                _id
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
+        })
+        describe('accessing threads from users', ()=>{
+            it('Should allow you to access threads', ()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                            threads{
+                                _id
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('Should not return userName on threads', ()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                            threads{
+                                _id
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
+        })
+        describe('accessing topics from users', ()=>{
+            it('Should allow you to access topics', ()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                            topics{
+                                _id
+                            }
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('Should not return userName on topics', ()=>{
+                const query = `
+                    {
+                        users{
+                            _id
+                            userName
+                            topics{
+                                _id
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
+            
+        })
+        })
+ 
+    describe('user', ()=>{
+        describe('basic user query',()=>{
+            it('should be valid query',()=>{
+                const query = `
+                        {
+                            user(id: ""){
+                                _id
+                                userName
+                            }
+                        }
+                    `
+                tester.test(true, query)
+            })
+            it('should require id',()=>{
+                const query = `
+                        {
+                            user(){
+                                _id
+                                userName
+                            }
+                        }
+                    `
+                tester.test(false, query)
+            })
+            it('cant take invalid value',()=>{
+                const query = `
+                        {
+                            user(){
+                                _id
+                                userNae
+                            }
+                        }
+                    `
+                tester.test(false, query)
+            })
+        })
+        describe('user comments access',()=>{
+            it('should be able to get comments',()=>{
+                const query = `
+                        {
+                            user(id: ""){
+                                _id
+                                userName
+                                comments{
+                                    _id
+                                }
+                            }
+                        }
+                    `
+                tester.test(true, query)
+            })
+            it('cant take invalid comment value',()=>{
+                const query = `
+                        {
+                            user(){
+                                _id
+                                userName
+                                comments{
+                                    _id
+                                    r
+                                }
+                            }
+                        }
+                    `
+                tester.test(false, query)
+            })
+        })
+        describe('user threads access',()=>{
+            it('should be able to get threads',()=>{
+                const query = `
+                        {
+                            user(id: ""){
+                                _id
+                                userName
+                                threads{
+                                    _id
+                                }
+                            }
+                        }
+                    `
+                tester.test(true, query)
+            })
+            it('cant take invalid thread value',()=>{
+                const query = `
+                        {
+                            user(){
+                                _id
+                                userName
+                                comments{
+                                    _id
+                                    r
+                                }
+                            }
+                        }
+                    `
+                tester.test(false, query)
+            })
+        })
+        describe('user topic access',()=>{
+            it('should be able to get topics',()=>{
+                const query = `
+                        {
+                            user(id: ""){
+                                _id
+                                userName
+                                topics{
+                                    _id
+                                }
+                            }
+                        }
+                    `
+                tester.test(true, query)
+            })
+            it('cant take invalid topic value',()=>{
+                const query = `
+                        {
+                            user(){
+                                _id
+                                userName
+                                topics{
+                                    _id
+                                    r
+                                }
+                            }
+                        }
+                    `
+                tester.test(false, query)
+            })
         })
     })
 })
