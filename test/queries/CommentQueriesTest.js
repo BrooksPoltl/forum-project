5f30083a5a4192deaf6d9ebd0ef8e1efffbacd84
BrooksPoltl <bpoltl1@gmail.com>
2019-06-09 17:40:57 -0500

    comments test

--- test/queries/CommentQueriesTest.js
+++ test/queries/CommentQueriesTest.js
@@ -2,11 +2,118 @@ const schemaCode = require('../../backend/schema/schema')
 const EasyGraphQLTester = require('easygraphql-tester')
 
 describe('Comment Queries', ()=>{
+    let tester;
+    before(()=>{
+        tester = new EasyGraphQLTester(schemaCode)
+    })
     describe('Comments', ()=>{
         describe('basic comments query', ()=>{
-
+            it('should be valid query', ()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                        }
+                    }
+                `
+                tester.test(true, query)
+            })
+            it('should be invalid query', ()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            das
+                        }
+                    }
+                `
+                tester.test(false, query)
+            })
         })
-        
+        describe('access users from upvotes', ()=>{
+            it('should be able to access user data',()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            upvotes{
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(true,query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            upvotes{
+                                userNam
+                            }
+                        }
+                    }
+                `
+                tester.test(false,query)
+            })
+        })
+        describe('access users from downvotes', ()=>{
+            it('should be able to access user data',()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            downvotes{
+                                userName
+                            }
+                        }
+                    }
+                `
+                tester.test(true,query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            downvotes{
+                                userNam
+                            }
+                        }
+                    }
+                `
+                tester.test(false,query)
+            })
+        })
+        describe('access thread', ()=>{
+            it('should be able to access thread data',()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            thread{
+                                title
+                            }
+                        }
+                    }
+                `
+                tester.test(true,query)
+            })
+            it('should not take invalid value',()=>{
+                const query = `
+                    {
+                        comments{
+                            _id
+                            thread{
+                                titl
+                            }
+                        }
+                    }
+                `
+                tester.test(false,query)
+            })
+        })
     })
     describe('Comment', ()=> {
 
