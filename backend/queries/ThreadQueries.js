66cd585641a119e5bb85b180925c1c2b1840d79b
BrooksPoltl <bpoltl1@gmail.com>
2019-07-06 23:31:04 -0500

    working on lint rules for thread queries

--- backend/queries/ThreadQueries.js
+++ backend/queries/ThreadQueries.js
@@ -1,29 +1,30 @@
-const {ThreadType}=require('../types/ThreadType')
-const Thread= require('../models/thread')
 
-const graphql = require('graphql')
+const graphql = require('graphql');
 
-const {GraphQLList, GraphQLNonNull, GraphQLID}=graphql;
+const { ThreadType } = require('../types/ThreadType');
+const Thread = require('../models/thread');
+
+const { GraphQLList, GraphQLNonNull, GraphQLID } = graphql;
 
 const getThreadById = {
-        type: GraphQLList(ThreadType),
-        args:{id: {type: new GraphQLNonNull(GraphQLID)}},
-        async resolve(parentValue, args){
-            return Thread.find({_id: args.id })
+	type: GraphQLList(ThreadType),
+    args: { id: { type: new GraphQLNonNull(GraphQLID) } },
+        async resolve(parentValue, args) {
+            return Thread.find({_id: args.id });
         }
     }
-const getThreads ={
+const getThreads = {
     type: GraphQLList(ThreadType),
     args: {},
-    resolve(parentValue, args){
+    resolve() {
         return Thread.find()
-        .then(threads=>{
-            return threads.map(thread =>{
-                return {...thread._doc, id: thread._id}
+        .then((threads) => {
+            return threads.map((thread) => {
+                return { ...thread._doc, id: thread._id };
             })
-        }).catch(err=>{
+        }).catch((err) => {
             throw err;
-        })
+        });
     }
 }
 
