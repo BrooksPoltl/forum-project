cf156ebd91f7364abf4f1cc1fb7d9b8db0e68201
BrooksPoltl <bpoltl1@gmail.com>
2019-07-08 16:27:06 -0500

    added comment type refactor

--- backend/types/CommentType.js
+++ backend/types/CommentType.js
@@ -20,28 +20,50 @@ const CommentType= new GraphQLObjectType({
     return{
         _id: {type: new GraphQLNonNull(GraphQLID)},
         content: {type: new GraphQLNonNull(GraphQLString)},
-        upvotes: {type: new GraphQLNonNull(GraphQLList(UserType)),resolve:async(parentValue, args)=>{
-            let result = await Comment.findById(parentValue._id)
-            return [...result.upvotes]
+        upvotes: {type: new GraphQLNonNull(GraphQLList(UserType)),
+            resolve:async(parentValue, args)=>{
+                let result = await Comment.findById(parentValue._id);
+                let upvoteIds = result.upvotes;
+                let response = []
+
+                for(let i = 0; i<upvoteIds.length; i++){
+                    let currentUpvote = await User.findById(upvoteIds[i])
+                    response.push(currentUpvote);
+                }
+                return response
         }},
-        downvotes: {type: new GraphQLNonNull(GraphQLList(UserType)),resolve:async(parentValue,args)=>{
-            let result = await Comment.findById(parentValue._id)
-            return [...result.downvotes]
+        downvotes: {type: new GraphQLNonNull(GraphQLList(UserType)),
+            resolve:async(parentValue,args)=>{
+                let result = await Comment.findById(parentValue._id);
+                let downvoteIds = result.downvotes;
+                let response = [];
+
+                for(let i = 0; i<downvoteIds.length; i++){
+                    let currentDownvote = await User.findById(downvoteIds[i]);
+                    response.push(currentDownvote);
+                }
+                return response
         }},
         total: {type: new GraphQLNonNull(GraphQLInt), resolve:(parentValue, args)=>{
             let tally = parentValue.upvotes.length - parentValue.downvotes.length
             return tally
         }},
-        user: {type: new GraphQLNonNull(GraphQLID)},
+        user: {type: new GraphQLNonNull(UserType),
+            resolve:async(parentValue, args)=>{
+                const result = await Comment.findById(parentValue._id);
+                const user = await User.findById(result.userId);
+                return user
+        }},
         thread: {type: new GraphQLNonNull(ThreadType),
             resolve:async(parentValue, args)=>{
                 let result = await Comment.findById(parentValue._id)
                 return result.thread
             }},
-        createdAt:{type: new GraphQLNonNull(GraphQLString),resolve:async(parentValue,args)=>{
-            let result = await Thread.findById(parentValue._id)
-            let answer = result.createdAt.toString()
-            return answer
+        createdAt:{type: new GraphQLNonNull(GraphQLString),
+            resolve:async(parentValue,args)=>{
+                let result = await Comment.findById(parentValue._id)
+                let answer = result.createdAt.toString()
+                return answer
             }}
     }}
 })
