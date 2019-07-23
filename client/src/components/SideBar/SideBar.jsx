09aaedd68172fabe8290d3be5624fed585b2b4a2
BrooksPoltl <bpoltl1@gmail.com>
2019-07-22 22:25:45 -0500

    commiting

--- client/src/components/SideBar/SideBar.jsx
+++ client/src/components/SideBar/SideBar.jsx
@@ -2,11 +2,8 @@ import React from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import { withStyles } from '@material-ui/styles';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
+
 import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
